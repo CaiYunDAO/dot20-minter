24475bf1d854bbe2a50de22417e33415292f8921
liwei <liwei-8466@qq.com>
2023-12-22 18:36:01 +0800

    fix num as success num

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
   // 这里填自己的助记词
   const my = keyring.addFromUri("action");
 
+  // 286275
   async function mint(token) {
     const tx1 = api.tx.balances.transferKeepAlive(my.address, 0);
     const tx2 = api.tx.system.remark(
@@ -18,31 +19,32 @@ import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
   }
 
   // 这里填需要铸造的数量
-  const num = 100;
+  const num = 1;
   // 这里填需要铸造的token名称
   const token = "DOTA";
   // 这里填锻造成功之后等待的时间，单位为秒
-  const wait = 5;
+  const wait = 6;
 
-  let success = 0;
   console.log(`开始铸造 ${token}，铸造数量为 ${num}`);
   console.log("====================================");
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < num; ) {
     try {
       console.log(`开始铸造第${i + 1}个`);
       const mintHash = await mint("DOTA");
       if (mintHash.hash.toHex().length !== 0) {
-        success++;
+        num++;
         console.log(`铸造上链成功，hash: ${mintHash}，等待${wait}秒`);
+      } else {
+        throw new Error("fail");
       }
     } catch (e) {
       console.log(`铸造失败，等待${wait}秒`);
     } finally {
-      await sleep(wait*1000);
+      if (i !== num - 1) {
+        await sleep(wait * 1000);
+      }
     }
   }
-  console.log("====================================");
-  console.log(`铸造完成，成功铸造 ${success} 个`);
 })();
 
 function sleep(ms) {
